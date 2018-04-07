9dc480088fd7c908a7e37d6a60439f6f8e0bb3bd
Andrew Smith <als5ev@virginia.edu>
2018-04-07 18:19:18 -0400

    PUTS embedded tweets and no more gibberish root in fb db

--- flask_app/sentiment.py
+++ flask_app/sentiment.py
@@ -3,6 +3,7 @@ from proxy_handler import get_proxy
 from textblob import TextBlob
 import sqlite3
 import json
+import requests
 
 def get_sentiment(name):
 
@@ -10,7 +11,7 @@ def get_sentiment(name):
     for i in range(0, 50):
         try:
             proxy = get_proxy()
-            tweetCriteria = got3.manager.TweetCriteria().setQuerySearch(name).setMaxTweets(100)
+            tweetCriteria = got3.manager.TweetCriteria().setQuerySearch(name).setMaxTweets(10)
             tweets = got3.manager.TweetManager.getTweets(tweetCriteria, proxy=proxy)
             break
         except: #Twitter has a lot of countermeasures around this so it is easier to not specify every exception
@@ -39,18 +40,22 @@ def get_sentiment(name):
     top_tweets = []
 
 
-    for i in range(-1, -11, -1):
+    for i in range(-1, -12, -1):
         if ((i * -1)-1) < len(tweets)//2:
-            positive_tweets.append(all_info[i][0].id)
+            tweet_id = all_info[i][0].id
+            r = requests.get("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F"+str(tweet_id))
+            positive_tweets.append(str(r.content)[2:])
             #print(all_info[i][0].text)
         else:
             break
 
     #print()
 
-    for i in range(0, len(tweets)):
+    for i in range(0, 10):
         if i < len(tweets)//2:
-            negative_tweets.append(all_info[i][0].id)
+            tweet_id = all_info[i][0].id
+            r = requests.get("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F"+str(tweet_id))
+            negative_tweets.append(str(r.content)[2:])
             #print(all_info[i][0].text)
         else:
             break
@@ -59,9 +64,11 @@ def get_sentiment(name):
 
     all_info.sort(key=lambda x:x[3])
 
-    for i in range(-1, -11, -1):
+    for i in range(-1, -12, -1):
         if ((i*-1)-1) < len(tweets) // 2:
-            top_tweets.append(all_info[i][0].id)
+            tweet_id = all_info[i][0].id
+            r = requests.get("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F"+str(tweet_id))
+            top_tweets.append(str(r.content)[2:])
             #print(all_info[i][0].text)
         else:
             break
@@ -80,23 +87,24 @@ def get_sentiment(name):
     #print()
     #print(top_tweets)
 
-    charity_dict = {'name': name, 'sentiment':mean_sentiment, 'positive':positive_tweets, 'negative':negative_tweets, 'top':top_tweets}
+
+    charity_dict = {'sentiment':mean_sentiment, 'positive':positive_tweets, 'negative':negative_tweets, 'top':top_tweets, 'quantity':len(tweets)}
 
     json_dict = json.dumps(charity_dict)
 
     json_dict = json.loads(json_dict)
 
     from firebase import firebase
-    firebase = firebase.FirebaseApplication('https://charitycheck-check.firebaseio.com/', None)
+    firebase = firebase.FirebaseApplication("https://charity-check.firebaseio.com/", None)
 
     #r = requests.post("https://charitycheck-check.firebaseio.com/", data=json_dict)
 
-    result = firebase.post("/charities", json_dict)
+    result = firebase.put("", name, json_dict)
 
     print(json_dict)
     print(type(json_dict))
 
 
 if __name__ == "__main__":
-    print(get_sentiment("Andrew"))
+    print(get_sentiment("Victoria"))
 
