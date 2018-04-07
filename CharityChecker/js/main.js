827d6fe7103a728bce4458ce56e57db208b6cbff
tomheatwole <tgh8xn@virginia.edu>
2018-04-06 21:50:03 -0400

    Adding AJAX call

--- CharityChecker/js/main.js
+++ CharityChecker/js/main.js
@@ -1,3 +1,10 @@
 function search(){
-	  alert('Search button pressed');
+    $.ajax({
+        type: "GET",
+        dataType: "JSON",
+        url: "https://console.firebase.google.com/project/charitycheck-check/database/charitycheck-check/data",
+        success: function(data) {
+            console.log(data);
+        }
+    });
 }
